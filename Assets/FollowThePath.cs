8296a644b9f8839032c3be88351646eb6b31d8a3
Rez <r.atae@yahoo.com>
2020-11-23 14:24:12 +0000

    first game commit

---
+++ Assets/FollowThePath.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowThePath : MonoBehaviour
+{
+    public Transform[] WayPoints;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    [HideInInspector]
+    public int WayPointIndex = 0;
+
+    public bool moveAllowed = false;
+
+    private void Start()
+    {
+        transform.position = WayPoints[WayPointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        if (moveAllowed)
+            Move();
+    }
+
+    private void Move()
+    {
+        if (WayPointIndex <= WayPoints.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, WayPoints[WayPointIndex].transform.position,
+            moveSpeed * Time.deltaTime);
+
+            if (transform.position == WayPoints[WayPointIndex].transform.position)
+            {
+                WayPointIndex += 1;
+            }
+        }
+    }
+
+
+}
---
+++ Assets/GameControl.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+   private static GameObject whoWinsTextShadow, player1MoveText, player2MoveText;
+   private static GameObject player1, player2;
+
+   public static int diceSideThrown = 0;
+   public static int player1StartWaypoint = 0;
+   public static int player2StartWaypoint = 0;
+
+   public static bool gameOver = false;
+
+   void Start () {
+
+        whoWinsTextShadow = GameObject.Find("whoWinsText");
+        player1MoveText = GameObject.Find("Player1MoveText");
+        player2MoveText = GameObject.Find("Player2MoveText");
+
+        player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
+
+        player1.GetComponent<FollowThePath>().moveAllowed = false;
+        player2.GetComponent<FollowThePath>().moveAllowed = false;
+
+        whoWinsTextShadow.gameObject.SetActive(false);
+        player1MoveText.gameObject.SetActive(true);
+        player2MoveText.gameObject.SetActive(false);
+   }
+
+   void Update()
+   {
+       if (player1.GetComponent<FollowThePath>().WayPointIndex >
+           player1StartWaypoint + diceSideThrown)
+       {
+
+           player1.GetComponent<FollowThePath>().moveAllowed = false;
+           player1MoveText.gameObject.SetActive(false);
+           player2MoveText.gameObject.SetActive(true);
+           player1StartWaypoint = player1.GetComponent<FollowThePath>().WayPointIndex - 1;
+
+       }
+
+       if (player2.GetComponent<FollowThePath>().WayPointIndex >
+           player2StartWaypoint + diceSideThrown)
+       {
+           player2.GetComponent<FollowThePath>().moveAllowed = false;
+           player2MoveText.gameObject.SetActive(false);
+           player1MoveText.gameObject.SetActive(true);
+           player2StartWaypoint = player2.GetComponent<FollowThePath>().WayPointIndex - 1;
+       }
+
+       if (player1.GetComponent<FollowThePath>().WayPointIndex ==
+           player1.GetComponent<FollowThePath>().WayPoints.Length)
+       {
+           whoWinsTextShadow.gameObject.SetActive(true);
+           whoWinsTextShadow.GetComponent<Text>().text = "Player 1 wins";
+           gameOver = true;
+       }
+
+        if (player2.GetComponent<FollowThePath>().WayPointIndex ==
+            player2.GetComponent<FollowThePath>().WayPoints.Length)
+        {
+            whoWinsTextShadow.gameObject.SetActive(true);
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(false);
+            whoWinsTextShadow.GetComponent<Text>().text = "Player 2 wins";
+            gameOver = true;
+        }
+    }
+
+   public static void MovePlayer(int playerToMove)
+   {
+        switch (playerToMove){
+            case 1:
+                player1.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+
+            case 2:
+                player2.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+        }
+   }
+}
